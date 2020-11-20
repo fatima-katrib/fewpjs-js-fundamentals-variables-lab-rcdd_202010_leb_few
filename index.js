f7855330ecb96ba2a46db1c50de4d6d44aa17a58
jovial-application-1943 <fatima.katrib@lau.edu>
2020-11-20 20:05:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
-const companyName
 let companyCeo = 'Suzan Smith';
 describe('index.js', function () {
   // there's stuff in here, too
