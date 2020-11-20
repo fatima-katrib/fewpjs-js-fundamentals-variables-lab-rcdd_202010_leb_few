965a7ce6a086c1d33e1b7017227322671ded1bb1
jovial-application-1943 <fatima.katrib@lau.edu>
2020-11-20 20:11:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 const companyName = 'Scuber';
-let companyCeo = 'Suzan Smith';
+let companyCeo = 'Susan Smith';
 let mostProfitableNeighborhood ='Chelsea';
 describe('index.js', function () {
   // there's stuff in here, too
